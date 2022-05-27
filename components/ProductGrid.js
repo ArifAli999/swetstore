470a31e4ece22f533d8be246fad30e950236fb92
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-05-27 05:29:51 +0100

    upds

--- components/ProductGrid.js
+++ components/ProductGrid.js
@@ -6,9 +6,7 @@ function ProductGrid({ products, ...props }) {
   return (
     <div className='grid md:grid-cols-4 grid-cols-1 gap-10 mb-10 mt-5 '>
 
-      {products.map((product) => (
-        <Product key={product.id} {...product} {...props} />
-      ))}
+   
     </div>
   );
 }
