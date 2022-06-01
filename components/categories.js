4bb2f65334f5963c8205686b138c749cc62275e9
Riffyraff <82163631+ArifAli999@users.noreply.github.com>
2022-06-01 01:12:18 +0100

    fixes

--- components/categories.js
+++ components/categories.js
@@ -26,64 +26,67 @@ export const Categories = () => {
 
   return (
     <><div className='grid md:grid-cols-3 gap-10 items-center align-middle self-center justify-items-center mt-10'>
-
+   <Link href='/categories/sweets'><button className='p-6'>Hiii</button></Link> 
       {cats.map((c) => (
-        <motion.div
-          key={c.id}
-          initial={{ opacity: 1, y: 100 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              delay: 1.25,
-            },
-          }}
-          className=' group h-full w-full bg-gray-400 shadow-xl cursor-pointer opacity-90 hover:opacity-100 transition-all ease-in-out duration-500 overflow-hidden '>
-          <div className=' grid grid-cols-5 justify-items-center justify-center items-center align-middle w-full h-full '
-          >
+      
+          <motion.div
+            key={c.id}
+            initial={{ opacity: 1, y: 100 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: 1.25,
+              },
+            }}
+            className=' group h-full w-full bg-gray-400 shadow-xl cursor-pointer opacity-90 hover:opacity-100 transition-all ease-in-out duration-500 overflow-hidden '>
 
-            <div className='col-span-3  align-middle  p-1.5 rounded float-right ml-20'>
-              <motion.div
-                whileHover={{
-                  scale: 1.1,
-                }}
+          
+            <div className=' grid grid-cols-5 justify-items-center justify-center items-center align-middle w-full h-full '
+            >
 
-
-                className='  text-gray-300 text-center z-40 group-hover:text-gray-50 transition-all duration-300 ease-in-out p-2 text-4xl font-serif font-black'>
-
-                <a href={`/categories/${c.slug}`}> {c.name}</a>
+              <div className='col-span-3  align-middle  p-1.5 rounded float-right ml-20'>
+                <motion.div
+                  whileHover={{
+                    scale: 1.1,
+                  }}
 
 
-              </motion.div>
+                  className='  text-gray-300 text-center z-40 group-hover:text-gray-50 transition-all duration-300 ease-in-out p-2 text-4xl font-serif font-black'>
 
+                  <a href={`/categories/${c.slug}`}> {c.name}</a>
+
+
+                </motion.div>
+
+              </div>
+              <div className='col-span-2'>
+
+                <motion.div
+                  whileHover={{
+                    scale: 1.2,
+                    transition: { duration: 1 }
+                  }}
+                  className='float-right w-56 h-56	relative overflow-hidden'>
+                  <img src='https://i.imgur.com/36IhkNN.png' className='h-52 w-52 absolute top-10 left-24 md:left-20 transform origin-top-left group-hover:scale-150 transition-all duration-500 ease-in-out  '></img>
+
+                </motion.div>
+
+              </div>
             </div>
-            <div className='col-span-2'>
-
-              <motion.div
-                whileHover={{
-                  scale: 1.2,
-                  transition: { duration: 1 }
-                }}
-                className='float-right w-56 h-56	relative overflow-hidden'>
-                <img src='https://i.imgur.com/36IhkNN.png' className='h-52 w-52 absolute top-10 left-24 md:left-20 transform origin-top-left group-hover:scale-150 transition-all duration-500 ease-in-out  '></img>
-
-              </motion.div>
-
-            </div>
-          </div>
 
 
 
-        </motion.div>
+          </motion.div>
 
-      ))}
+          ))}
 
 
 
 
 
 
-    </div><hr className="w-1/2 mx-auto h-full mt-16" /></>
+        </div><hr className="w-1/2 mx-auto h-full mt-16" /></>
   )
 }
 
